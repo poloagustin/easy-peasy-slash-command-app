9e772ea7dfa623ee3582670ceeeaf7df646b3c60
Agustin Polo <agustinpolo@Agustins-MacBook-Pro.local>
2017-03-18 11:56:52 -0300

    first commit

--- index.js
+++ index.js
@@ -42,17 +42,19 @@
 
 /* Uses the slack button feature to offer a real time bot to multiple teams */
 var Botkit = require('botkit');
-
 if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET || !process.env.PORT || !process.env.VERIFICATION_TOKEN) {
     console.log('Error: Specify CLIENT_ID, CLIENT_SECRET, VERIFICATION_TOKEN and PORT in environment');
     process.exit(1);
 }
 
 var config = {}
-if (process.env.MONGOLAB_URI) {
-    var BotkitStorage = require('botkit-storage-mongo');
+if (process.env.DATABASE_URL) {
+    const firebaseStorage = require('botkit-storage-firebase')({
+        firebase_uri: process.env.DATABASE_URL
+    });
+
     config = {
-        storage: BotkitStorage({mongoUri: process.env.MONGOLAB_URI}),
+        storage: firebaseStorage,
     };
 } else {
     config = {
@@ -86,35 +88,81 @@ controller.setupWebserver(process.env.PORT, function (err, webserver) {
 //
 
 controller.on('slash_command', function (slashCommand, message) {
-
     switch (message.command) {
-        case "/echo": //handle the `/echo` slash command. We might have others assigned to this app too!
+        case "/panadero": //handle the `/echo` slash command. We might have others assigned to this app too!
             // The rules are simple: If there is no text following the command, treat it as though they had requested "help"
             // Otherwise just echo back to them what they sent us.
 
             // but first, let's make sure the token matches!
             if (message.token !== process.env.VERIFICATION_TOKEN) return; //just ignore it.
 
-            // if no text was supplied, treat it as a help command
-            if (message.text === "" || message.text === "help") {
-                slashCommand.replyPrivate(message,
-                    "I echo back what you tell me. " +
-                    "Try typing `/echo hello` to see.");
+            if (message.text === 'help') {
+                slashCommand.replyPrivate(
+                    message,
+                `<empty> - Quien tiene que comprar?\n
+                add <name> - Agregar alguien a la lista\n
+                remove <name> - Sacar alguien de la lista\n
+                change <from> <to> - Cambiar el nombre de alguien`);
+                return;
+            } else if (message.text === '') {
+                // slashCommand.replyPrivate(
+                //     message,
+                //     'Buscando deudores...',
+                //     () => controller.storage.teams.get(
+                //         'newcomers', 
+                //         (x, newcomers) => slashCommand.replyPrivateDelayed(message, formatNewcomersText(newcomers))));
+                controller.storage.teams.get(
+                    'newcomers', 
+                    (x, newcomers) => slashCommand.replyPrivate(message, formatNewcomersText(newcomers)));
+                return;
+            } else if (!message.text.indexOf('add')) {
+                const name = message.text.replace('add ', '').trim();
+                controller.storage.teams.get('newcomers', (x, newcomers) => {
+                    newcomers.newcomers = newcomers.newcomers || [];
+                    newcomers.newcomers.push(name);
+                    controller.storage.teams.save(newcomers, () => 
+                        slashCommand.replyPrivate(message, `Agregue a ${name}!`)
+                    );
+                });
+            } else if (!message.text.indexOf('remove')) {
+                const name = message.text.replace('remove ', '').trim();
+                controller.storage.teams.get('newcomers', (x, newcomers) => {
+                    newcomers.newcomers = newcomers.newcomers || [];
+                    const indexOfName = newcomers.newcomers.indexOf(name);
+                    if (indexOfName > -1) {
+                        newcomers.newcomers.splice(indexOfName, 1);
+                        controller.storage.teams.save(newcomers, () => slashCommand.replyPrivate(message, `Removi a ${name}!`));
+                    } else {
+                        slashCommand.replyPrivate(message, `${name} no existe`);
+                    }
+                });
+            } else if (!message.text.indexOf('rename')) {
+                const names = message.text.replace('rename ', '').trim();
+                const namesArray = names.split(' ');
+                const from = namesArray[0], to = namesArray[1];
+
+                controller.storage.teams.get('newcomers', (x, newcomers) => {
+                    newcomers.newcomers = newcomers.newcomers || [];
+                    const indexOfName = newcomers.newcomers.indexOf(from);
+                    if (indexOfName > -1) {
+                        newcomers.newcomers[indexOfName] = to;
+                        controller.storage.teams.save(newcomers, () => slashCommand.replyPrivate(message, `${from} ahora se llama ${to}!`));
+                    } else {
+                        slashCommand.replyPrivate(message, `${from} no existe`);
+                    }
+                });
+            } else {
+                slashCommand.replyPrivate(message, "Don't really know what to do with `" + message.text + "`");
                 return;
             }
-
-            // If we made it here, just echo what the user typed back at them
-            //TODO You do it!
-            slashCommand.replyPublic(message, "1", function() {
-                slashCommand.replyPublicDelayed(message, "2").then(slashCommand.replyPublicDelayed(message, "3"));
-            });
-
             break;
         default:
             slashCommand.replyPublic(message, "I'm afraid I don't know how to " + message.command + " yet.");
-
     }
 
-})
-;
+function formatNewcomersText(value) {
+    const newcomers = value.newcomers || [];
+    return newcomers.length ? `ATENCION!\nLos que todavia no compraron comida son:\n- ${newcomers.join('\n- ')}` : 'Nadie debe facturas...';
+}
+});
 
