b487b8a670fb6a3d939b5019f7d10011611c7696
Agustin Polo <agustinpolo@Agustins-MacBook-Pro.local>
2017-04-29 19:32:07 -0300

    algo mas

--- intents.js
+++ intents.js
@@ -1,9 +1,14 @@
 const builder = require('botbuilder');
 
-const intents = new builder.IntentDialog();
+const luisRecognizer = new builder.LuisRecognizer('https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/b965890c-d40b-4d16-bfdb-a926759f20d4?subscription-key=4a373a52f2aa42c788e0319f735b8d37&verbose=true&timezoneOffset=-3.0&q=');
 
-intents.matches(/add/, require('./waterfalls/create.waterfall'));
+const intentDialog = new builder.IntentDialog({
+    recognizers: [luisRecognizer]
+});
 
-intents.onDefault(require('./waterfalls/default.waterfall'));
+intentDialog.matches('create', require('./waterfalls/create.waterfall'));
+intentDialog.matches('list', require('./waterfalls/list.waterfall'));
 
-module.exports = intents;
+intentDialog.onDefault(require('./waterfalls/default.waterfall'));
+
+module.exports = intentDialog;
